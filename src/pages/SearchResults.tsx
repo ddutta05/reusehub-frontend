cc38523261db569d58224447d4df553c9e2e2daf
ddutta05 <drguuuu@gmail.com>
2025-09-22 14:54:21 +0600

    Updated SearchResults.tsx

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -119,6 +119,7 @@ const SearchResults = () => {
       setSelectedCategories(prev => prev.filter(cat => cat !== categoryName));
     }
   };
+
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
@@ -289,24 +290,6 @@ const SearchResults = () => {
                 </button>
               </div>
             )}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default SearchResults;
-                <ItemCard key={item.id} item={item} />
-              ))}
-            </div>
-
-            {/* Load More */}
-            <div className="text-center mt-8">
-              <button className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-lg font-semibold transition-colors">
-                Load More Items
-              </button>
-            </div>
           </div>
         </div>
       </div>
